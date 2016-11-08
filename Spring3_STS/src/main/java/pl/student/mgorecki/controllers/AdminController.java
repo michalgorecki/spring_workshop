394ad85b347c8521ca23b747e323bdd5f865fe3c
Michal Gorecki <mih.gorecki@gmail.com>
2016-11-08 16:02:54 +0100

    Customized login page

--- Spring3_STS/src/main/java/pl/student/mgorecki/controllers/AdminController.java
+++ Spring3_STS/src/main/java/pl/student/mgorecki/controllers/AdminController.java
@@ -3,7 +3,7 @@ package pl.student.mgorecki.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -15,6 +15,23 @@ public class AdminController {
 		model.addObject("title","Spring security hello world");
 		model.addObject("message","This is a protected view");
 		model.setViewName("admin");
+		return model;
+	}
+	
+	@RequestMapping(value = "/login", method = RequestMethod.GET)
+	public ModelAndView login(
+			@RequestParam(value =  "error", required = false) String error,
+			@RequestParam(value = "logout" , required = false) String logout){
+		
+		ModelAndView model = new ModelAndView();
+		
+		if(error != null){
+			model.addObject("error", "Invalid username and password");
+		}
+		if(logout != null) {
+			model.addObject("msg","You've been logged out successfully");
+		}
+		model.setViewName("login");
 		return model;
 	}
 
